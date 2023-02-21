f178e074d9f2a7969396c7bc7195b9d2f0ba7c8b
myConsciousness <contact@shinyakato.dev>
2023-02-22 08:22:54 +0900

    test: add mock

---
+++ test/mock/client_stubs.dart
@@ -0,0 +1,43 @@
+// Copyright 2022 Kato Shinya. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+// 🎯 Dart imports:
+import 'dart:io';
+
+// 📦 Package imports:
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+
+import 'mock.mocks.dart';
+
+MockClient buildPostStub(
+  final String instance,
+  final String unencodedPath,
+  final String resourcePath, {
+  required dynamic body,
+  int statusCode = 200,
+}) {
+  final mockClient = MockClient();
+  final requestUri = Uri.https(instance, unencodedPath, {});
+
+  when(mockClient.post(
+    requestUri,
+    headers: anyNamed('headers'),
+    body: body,
+  )).thenAnswer(
+    (_) async => Response(
+      await File(resourcePath).readAsString(),
+      statusCode,
+      headers: {
+        'content-type': 'application/json; charset=utf-8',
+      },
+      request: Request(
+        'POST',
+        requestUri,
+      ),
+    ),
+  );
+
+  return mockClient;
+}
---
+++ test/mock/mock.dart
@@ -0,0 +1,12 @@
+// Copyright 2023 Kato Shinya. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+// 🌎 Project imports:
+import 'package:misskey/src/core/client/client.dart';
+
+// 📦 Package imports:
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([Client])
+void main() {}
